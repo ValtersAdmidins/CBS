b164441ebccfa5f60e8b20758100131bb3a7b8e4
ValtersAdmidins <41115736+ValtersAdmidins@users.noreply.github.com>
2018-07-14 09:10:28 +0300

    ssagatave

--- src/main/java/com/booking/cinema/CinemaApplication.java
+++ src/main/java/com/booking/cinema/CinemaApplication.java
@@ -1,17 +1,8 @@
 package com.booking.cinema;
 
-import java.awt.print.Book;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.transaction.Transactional;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import com.booking.cinema.model.Auditorium;
-import com.booking.cinema.model.Cinema;
 
 @SpringBootApplication
 @EnableAutoConfiguration
@@ -20,4 +11,5 @@ public class CinemaApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CinemaApplication.class, args);
 	}
+	
 }
--- src/main/java/com/booking/cinema/repositories/MovieRepository.java
+++ src/main/java/com/booking/cinema/repositories/MovieRepository.java
@@ -1,11 +1,11 @@
-package com.booking.cinema.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-import com.booking.cinema.model.Movie;
-
-@Repository
-public interface MovieRepository extends JpaRepository<Movie, Long>{
-
-}
+//package com.booking.cinema.repositories;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import org.springframework.stereotype.Repository;
+//
+//import com.booking.cinema.model.Movie;
+//
+//@Repository
+//public interface MovieRepository extends JpaRepository<Movie, Long>{
+//
+//}
