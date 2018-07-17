3bf779b3c06146020e359721bc397cbd2e196536
ValtersAdmidins <41115736+ValtersAdmidins@users.noreply.github.com>
2018-07-17 14:04:04 +0300

    fix

--- src/main/java/com/booking/cinema/controller/TicketController.java
+++ src/main/java/com/booking/cinema/controller/TicketController.java
@@ -3,16 +3,9 @@ package com.booking.cinema.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-
-import org.springframework.web.bind.annotation.GetMapping;
-
-import com.booking.cinema.model.Auditorium;
-import com.booking.cinema.repositories.CinemaRepository;
-
-
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import com.booking.cinema.repository.TicketRepository;
+import com.booking.cinema.repositories.TicketRepository;
  
 @Controller
 public class TicketController {
