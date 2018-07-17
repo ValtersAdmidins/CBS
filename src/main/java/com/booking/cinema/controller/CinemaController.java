15967d0e8b346004e781fa80769cec58c9462102
ValtersAdmidins <41115736+ValtersAdmidins@users.noreply.github.com>
2018-07-17 20:52:12 +0300

    admin and user seperated working

--- src/main/java/com/booking/cinema/controller/CinemaController.java
+++ src/main/java/com/booking/cinema/controller/CinemaController.java
@@ -1,26 +1,24 @@
 package com.booking.cinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
-import com.booking.cinema.model.User;
+import com.booking.cinema.exceptions.ResourceNotFoundException;
+import com.booking.cinema.model.Cinema;
 import com.booking.cinema.repositories.CinemaRepository;
 import com.booking.cinema.repositories.UserRepository;
-
- 
 
 @Controller
 public class CinemaController {
 
 	@Autowired
 	CinemaRepository cinemaRepository;
-	
+
 	@Autowired
 	UserRepository userRepository;
 
@@ -29,48 +27,73 @@ public class CinemaController {
 		model.addAttribute("cinemas", cinemaRepository.findAll());
 		return "cinemas";
 	}
-
-/*
+	
 	@RequestMapping("/cinemas/{id}")
-	public String showAllMoviesinCinema(
-			@PathVariable(value = "id") Long cinemaID, Model model) {
-		Optional<Cinema> cinema = cinemaRepository.findById(cinemaID);
-		Set<Movie> movies = new HashSet<>();
-		if (cinema.isPresent()) {
-			movies = cinema.get().getMovies();
-			model.addAttribute("cinema", cinema.get());
-		}
-
-		model.addAttribute("movies", movies);
-
+	public String getCinemaById(@PathVariable(value = "id") Long cinemaId,
+			Model model) {
+		model.addAttribute("cinema",
+				cinemaRepository.findById(cinemaId).orElseThrow(
+						() -> new ResourceNotFoundException("Cinema", "id",
+								cinemaId)));
 		return "cinema";
 	}
-*/
-//	// Loads the cinama-create html page.
-//	@GetMapping("/cinemas/cinema-create")
-//	public String cinemaCreatePage(Model model) {
-//		model.addAttribute("cinema", new Cinema());
-//		return "cinema-create";
-//	}
-	
+
 	// Loads the cinema-create html page for admin.
-	@RequestMapping(value="/admin/cinemas/cinema-create", method = RequestMethod.GET)
-	public ModelAndView movieCreatePage(){
-		ModelAndView modelAndView = new ModelAndView();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userRepository.findByEmail(auth.getName());
-		modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-		modelAndView.setViewName("admin/cinema-create");
-		return modelAndView;
+	@GetMapping("/cinemas/cinema-create")
+	public String cinemaCreateForm(Model model) {
+		model.addAttribute("cinema", new Cinema());
+		return "admin/cinema-create";
 	}
 
-//	// Proccesses the cinema creation and insertion into the database.
-//	@PostMapping("/cinemas/cinema-create")
-//	public String createCinemaProccess(Cinema cinema) {
-//
-//		cinema.setLatitudeAndLongitudeFromAddress();
-//		cinemaRepository.save(cinema);
-//		return "redirect:/cinemas";
-//	}
+	// Proccesses the cinema creation and insertion into the database.
+	@PostMapping("/cinemas/cinema-create")
+	public String cinemaCreateProccess(Cinema cinema) {
+		cinema.setLatitudeAndLongitudeFromAddress();
+		cinemaRepository.save(cinema);
+		return "redirect:/cinemas";
+	}
+
+	/*
+	 * @RequestMapping("/cinemas/{id}") public String showAllMoviesinCinema(
+	 * 
+	 * @PathVariable(value = "id") Long cinemaID, Model model) {
+	 * Optional<Cinema> cinema = cinemaRepository.findById(cinemaID); Set<Movie>
+	 * movies = new HashSet<>(); if (cinema.isPresent()) { movies =
+	 * cinema.get().getMovies(); model.addAttribute("cinema", cinema.get()); }
+	 * 
+	 * model.addAttribute("movies", movies);
+	 * 
+	 * return "cinema"; }
+	 */
+	// // Loads the cinama-create html page.
+	// @GetMapping("/cinemas/cinema-create")
+	// public String cinemaCreatePage(Model model) {
+	// model.addAttribute("cinema", new Cinema());
+	// return "cinema-create";
+	// }
+
+	// // Loads the cinema-create html page for admin.
+	// @RequestMapping(value="/admin/cinemas/cinema-create", method =
+	// RequestMethod.GET)
+	// public ModelAndView movieCreatePage(){
+	// ModelAndView modelAndView = new ModelAndView();
+	// Authentication auth =
+	// SecurityContextHolder.getContext().getAuthentication();
+	// User user = userRepository.findByEmail(auth.getName());
+	// modelAndView.addObject("userName", "Welcome " + user.getName() + " " +
+	// user.getLastName() + " (" + user.getEmail() + ")");
+	// modelAndView.addObject("adminMessage","Content Available Only for Users
+	// with Admin Role");
+	// modelAndView.setViewName("admin/cinema-create");
+	// return modelAndView;
+	// }
+
+	// // Proccesses the cinema creation and insertion into the database.
+	// @PostMapping("/cinemas/cinema-create")
+	// public String createCinemaProccess(Cinema cinema) {
+	//
+	// cinema.setLatitudeAndLongitudeFromAddress();
+	// cinemaRepository.save(cinema);
+	// return "redirect:/cinemas";
+	// }
 }
--- src/main/java/com/booking/cinema/controller/LoginController.java
+++ src/main/java/com/booking/cinema/controller/LoginController.java
@@ -1,15 +1,11 @@
 package com.booking.cinema.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.booking.cinema.model.User;
 import com.booking.cinema.repositories.UserRepository;
@@ -20,18 +16,14 @@ public class LoginController {
 	@Autowired
 	UserRepository userRepository;
 	
-	@RequestMapping(value="/registration", method = RequestMethod.GET)
-	public ModelAndView registration(){
-		ModelAndView modelAndView = new ModelAndView();
-		User user = new User();
-		modelAndView.addObject("user", user);
-		modelAndView.setViewName("registration");
-		return modelAndView;
+	@GetMapping
+	public String registrationPage(Model model) {
+		model.addAttribute("user", new User());
+		return "registration";
 	}
 	
-	@RequestMapping(value = "/registration", method = RequestMethod.POST)
-	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
-		ModelAndView modelAndView = new ModelAndView();
+	@PostMapping("/registration")
+	public String createNewUser(User user, BindingResult bindingResult) {
 		User userExists = userRepository.findByEmail(user.getEmail());
 		if (userExists != null) {
 			bindingResult
@@ -39,27 +31,48 @@ public class LoginController {
 							"There is already a user registered with the email provided");
 		}
 		if (bindingResult.hasErrors()) {
-			modelAndView.setViewName("registration");
+			return "redirect:/registration";
 		} else {
 			userRepository.save(user);
-			modelAndView.addObject("successMessage", "User has been registered successfully");
-			modelAndView.addObject("user", new User());
-			modelAndView.setViewName("registration");
 			
 		}
-		return modelAndView;
+		return "redirect:/registration";
 	}
 	
-	@RequestMapping(value="/admin/home", method = RequestMethod.GET)
-	public ModelAndView home(){
-		ModelAndView modelAndView = new ModelAndView();
-		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-		User user = userRepository.findByEmail(auth.getName());
-		modelAndView.addObject("userName", "Welcome " + user.getName() + " " + user.getLastName() + " (" + user.getEmail() + ")");
-		modelAndView.addObject("adminMessage","Content Available Only for Users with Admin Role");
-		modelAndView.setViewName("admin/home");
-		return modelAndView;
-	}
+//	@GetMapping("/movies/movie-create")
+//	public String movieCreateForm(Model model) {
+//		model.addAttribute("movie", new Movie());
+//		return "movie";
+//	}
 	
+//	@RequestMapping(value="/registration", method = RequestMethod.GET)
+//	public ModelAndView registration(){
+//		ModelAndView modelAndView = new ModelAndView();
+//		User user = new User();
+//		modelAndView.addObject("user", user);
+//		modelAndView.setViewName("registration");
+//		return modelAndView;
+//	}
+	
+//	@RequestMapping(value = "/registration", method = RequestMethod.POST)
+//	public ModelAndView createNewUser(@Valid User user, BindingResult bindingResult) {
+//		ModelAndView modelAndView = new ModelAndView();
+//		User userExists = userRepository.findByEmail(user.getEmail());
+//		if (userExists != null) {
+//			bindingResult
+//					.rejectValue("email", "error.user",
+//							"There is already a user registered with the email provided");
+//		}
+//		if (bindingResult.hasErrors()) {
+//			modelAndView.setViewName("registration");
+//		} else {
+//			userRepository.save(user);
+//			modelAndView.addObject("successMessage", "User has been registered successfully");
+//			modelAndView.addObject("user", new User());
+//			modelAndView.setViewName("registration");
+//			
+//		}
+//		return modelAndView;
+//	}
 
 }
