25f9d240efa5da8eb730dad4f523d99668421404
ValtersAdmidins <41115736+ValtersAdmidins@users.noreply.github.com>
2018-07-17 10:23:57 +0300

    changes to user and admin permissions

--- src/main/java/com/booking/cinema/controller/LoginController.java
+++ src/main/java/com/booking/cinema/controller/LoginController.java
@@ -20,12 +20,12 @@ public class LoginController {
 	@Autowired
 	private UserService userService;
 
-	@RequestMapping(value={"/login"}, method = RequestMethod.GET)
-	public ModelAndView login(){
-		ModelAndView modelAndView = new ModelAndView();
-		modelAndView.setViewName("login");
-		return modelAndView;
-	}
+//	@RequestMapping(value={"/"}, method = RequestMethod.GET)
+//	public ModelAndView login(){
+//		ModelAndView modelAndView = new ModelAndView();
+//		modelAndView.setViewName("/");
+//		return modelAndView;
+//	}
 	
 	
 	@RequestMapping(value="/registration", method = RequestMethod.GET)
