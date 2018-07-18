8e12bf607adf5c4fc476198cc56fceae215ccc0e
Gita <sticinska@gmail.com>
2018-07-18 10:33:47 +0300

    added attribute with all showtimes for /movie/{id} view (all showtimes for a specific movie

--- src/main/java/com/booking/cinema/controller/MovieController.java
+++ src/main/java/com/booking/cinema/controller/MovieController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import com.booking.cinema.exceptions.ResourceNotFoundException;
 import com.booking.cinema.model.Movie;
 import com.booking.cinema.repositories.MovieRepository;
+import com.booking.cinema.repositories.ShowtimeRepository;
 import com.booking.cinema.repositories.UserRepository;
 
 @Controller
@@ -22,6 +23,9 @@ public class MovieController {
 
 	@Autowired
 	UserRepository userRepository;
+	
+	@Autowired
+	ShowtimeRepository showtimeRepository;
 
 	@RequestMapping("/")
 	public String showAllMovies(Model model) {
@@ -36,6 +40,8 @@ public class MovieController {
 				movieRepository.findById(movieId).orElseThrow(
 						() -> new ResourceNotFoundException("Movie", "id",
 								movieId)));
+		
+		model.addAttribute("showtimes",showtimeRepository.findAllShowtimesForMovie(movieId));
 		return "movie";
 	}
 
