9d43fdec0515caac942e8acaf647406b8e558eee
Gita <sticinska@gmail.com>
2018-07-18 10:34:56 +0300

    added controller that shows all showtimes

--- src/main/java/com/booking/cinema/controller/ShowtimeController.java
+++ src/main/java/com/booking/cinema/controller/ShowtimeController.java
@@ -2,6 +2,8 @@ package com.booking.cinema.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.booking.cinema.repositories.ShowtimeRepository;
 
@@ -11,6 +13,12 @@ public class ShowtimeController {
 	@Autowired
 	ShowtimeRepository showtimeRepository;
 	
+	
+	@RequestMapping("/shotimes")
+	public String showAllMovies(Model model) {
+		model.addAttribute("showtimes", showtimeRepository.findAll());
+		return "showtimes";
+	}
 	
 	
 	
